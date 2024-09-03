c509e81689ec244bc2f20fd0f24c9c087cd2bd28
Angel Pérez <angel.perez@pragma.com.co>
2024-09-03 14:43:55 -0500

    first commit

---
+++ src/main/java/org/agperezb/bootcampmanagement/BootcampManagementApplication.java
@@ -0,0 +1,13 @@
+package org.agperezb.bootcampmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/agperezb/bootcampmanagement/BootcampManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agperezb.bootcampmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
